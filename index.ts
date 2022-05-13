9617e213f23f0124a01d3d8caa5456628789c75a
Gordon Sun <gordon.sun@pipe17.com>
2022-05-13 14:56:15 -0700

    condition structure draft 1

--- index.ts
+++ index.ts
@@ -1,5 +1,71 @@
+
+enum JSONTypes {
+    Null,
+    Boolean,
+    String,
+    Number,
+    Array,
+    Object,
+}
+
+class StringCondition {
+    regex: string
+}
+
+class NumberCondition {
+    eq?: number
+    gt?: number
+    lt?: number
+}
+
+class AggregateCondition {
+    valueCondition: ValueCondition
+    matchingElementCount: NumberCondition
+}
+
+class ValueAtIndexCondition {
+    index: number
+    valueCondition: ValueCondition
+}
+
+class ArrayCondition {
+    sizeCondition?: NumberCondition
+    aggregateCondition?: AggregateCondition
+    valueAtIndexCondition?: ValueAtIndexCondition
+}
+
+class FieldCondition {
+    fieldName: string
+    fieldValueCondition: ValueCondition
+}
+
+class ObjectCondition {
+    keysCondition?: ArrayCondition
+    valuesCondition?: ArrayCondition
+    fieldCondition?: FieldCondition
+}
+
+class ValueCondition {
+    allowedTypes?: JSONTypes[]
+    stringCondition?: StringCondition
+    numberCondition?: NumberCondition
+    arrayCondition?: ArrayCondition
+    objectCondition?: ObjectCondition
+}
+
+class Condition {
+    all?: Condition[]
+    any?: Condition[]
+    not?: Condition
+    valueCondition?: ValueCondition
+
+    constructor(input: object) {
+        console.log(typeof input)
+    }
+}
+
 const main = async () => {
-    console.log('main')
+    new Condition({})
 }
 
 main()
