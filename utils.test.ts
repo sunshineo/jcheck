877a2c391160fb2c2c0cb80bbc784420a738f30a
Gordon Sun <gordon.sun@pipe17.com>
2022-05-14 13:37:28 -0700

    Finish utils test

--- utils.test.ts
+++ utils.test.ts
@@ -8,3 +8,23 @@ it('boolean fails', () => {
     expect(objectNotArrayNotNull(true)).toEqual(false)
     expect(objectNotArrayNotNull(false)).toEqual(false)
 })
+
+it('number fails', () => {
+    expect(objectNotArrayNotNull(5)).toEqual(false)
+})
+
+it('string fails', () => {
+    expect(objectNotArrayNotNull('abc')).toEqual(false)
+})
+
+it('array fails', () => {
+    expect(objectNotArrayNotNull([])).toEqual(false)
+    expect(objectNotArrayNotNull([1,2,3])).toEqual(false)
+    expect(objectNotArrayNotNull(['a','b'])).toEqual(false)
+})
+
+it('object success', () => {
+    expect(objectNotArrayNotNull({})).toEqual(true)
+    expect(objectNotArrayNotNull({a:'b'})).toEqual(true)
+    
+})
