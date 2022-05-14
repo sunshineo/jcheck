3642e5e07551f575e43fb62b17c9a1c472d6611b
Gordon Sun <gordon.sun@pipe17.com>
2022-05-14 13:35:02 -0700

    boolean fails

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
-import FieldCondition from "./FieldCondition"
+import StringCondition from "./StringCondition"
 
 const main = async () => {
-
-    const cond1 = new FieldCondition({})
+    const cond1Json = {
+        eq: 'abc',
+        caseInsensitive: true,
+    }
+    const cond1 = new StringCondition(cond1Json)
+    console.log(cond1.check(""))
+    console.log(cond1.check("Ab"))
+    console.log(cond1.check("abc"))
 }
 
 main()
--- utils.test.ts
+++ utils.test.ts
@@ -3,3 +3,8 @@ import { objectNotArrayNotNull } from "./utils"
 it('null fails', () => {
     expect(objectNotArrayNotNull(null)).toEqual(false)
 })
+
+it('boolean fails', () => {
+    expect(objectNotArrayNotNull(true)).toEqual(false)
+    expect(objectNotArrayNotNull(false)).toEqual(false)
+})
