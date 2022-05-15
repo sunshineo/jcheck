ff42c367458ec6a66b61db18bdf1939c0c577511
Gordon Sun <gordon.sun@pipe17.com>
2022-05-14 17:13:09 -0700

    More boolean test

--- BooleanCondition.test.ts
+++ BooleanCondition.test.ts
@@ -1,9 +1,13 @@
 import BooleanCondition from "./BooleanCondition"
 
-describe('constructor works properly', () => {
-    it('throw if input is not proper object', () => {
-        expect(() => {
-            new BooleanCondition('not-object')
-        }).toThrow()
+describe('constructor', () => {
+    test('throw if input not object', () => {
+        expect(() => {new BooleanCondition('not-object')}).toThrow()
+    })
+    test('throw if input does not have eq', () => {
+        expect(() => {new BooleanCondition({})}).toThrow()
+    })
+    test('throw if eq is not boolean', () => {
+        expect(() => {new BooleanCondition({ eq: 'abc' })}).toThrow()
     })
 })
