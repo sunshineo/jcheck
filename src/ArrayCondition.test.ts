dd273aabadcd9cf94cec91c404ffb7a948da4ec7
Gordon Sun <gordon.sun@pipe17.com>
2022-05-16 00:52:01 -0700

    More tests

--- src/ArrayCondition.test.ts
+++ src/ArrayCondition.test.ts
@@ -28,7 +28,16 @@ describe ('ArrayCondition', () => {
             })
         })
         test('has multiple conditions should throw', () => {
-            expect(() => {new ArrayCondition({ fieldValue: "", fieldNames: "" })}).toThrow()
+            expect(() => {
+                new ArrayCondition({
+                    all: [{
+                        size: { eq: 0 }
+                    }],
+                    any: [{
+                        size: { eq: 0 }
+                    }]
+                })
+            }).toThrow()
         })
     })
     describe('check', () => {
@@ -39,6 +48,98 @@ describe ('ArrayCondition', () => {
                 }
             })
             expect(cond.check('not-array')).toBe(false)
+        })
+        describe('all', ()=> {
+            const cond = new ArrayCondition({
+                all: [
+                    {
+                        size: { gt: 0 }
+                    },
+                    {
+                        size: { lt: 2 }
+                    },
+                ]
+            })
+            test('return true', () => {
+                expect(cond.check(['one'])).toBe(true)
+            })
+            test('return false', () => {
+                expect(cond.check(['one', 'two'])).toBe(false)
+            })
+        })
+        describe('any', ()=> {
+            const cond = new ArrayCondition({
+                any: [
+                    {
+                        size: { gt: 2 }
+                    },
+                    {
+                        size: { lt: 1 }
+                    },
+                ]
+            })
+            test('return true', () => {
+                expect(cond.check([])).toBe(true)
+            })
+            test('return false', () => {
+                expect(cond.check(['one'])).toBe(false)
+            })
+        })
+        describe('not', () => {
+            const cond = new ArrayCondition({
+                not: {
+                    size: { eq: 0 }
+                }
+            })
+            test('return true', () => {
+                expect(cond.check(['one'])).toBe(true)
+            })
+            test('return false', () => {
+                expect(cond.check([])).toBe(false)
+            })
+        })
+        describe('size', () => {
+            const cond = new ArrayCondition({
+                size: { eq: 0 }
+            })
+            test('true', () => {
+                expect(cond.check([])).toBe(true)
+            })
+            test('false', () => {
+                expect(cond.check(['one'])).toBe(false)
+            })
+        })
+        describe('hasElement', () => {
+            const cond = new ArrayCondition({
+                hasElement: {
+                    allowedType: 'string',
+                    stringCondition: {
+                        eq: 'abc'
+                    }
+                }
+            })
+            test('true', () => {
+                expect(cond.check(['a', 'ab', 'abc'])).toBe(true)
+            })
+            test('false', () => {
+                expect(cond.check(['a', 'ab'])).toBe(false)
+            })
+        })
+        describe('hasNoElement', () => {
+            const cond = new ArrayCondition({
+                hasNoElement: {
+                    allowedType: 'string',
+                    stringCondition: {
+                        eq: 'abc'
+                    }
+                }
+            })
+            test('true', () => {
+                expect(cond.check(['a', 'ab'])).toBe(true)
+            })
+            test('false', () => {
+                expect(cond.check(['a', 'ab', 'abc'])).toBe(false)
+            })
         })
     })
 })
