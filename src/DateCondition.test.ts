d42219b7a693c8785c877e16789dbb9ba99b6a5b
Gordon Sun <gordon.sun@pipe17.com>
2022-05-16 02:50:54 -0700

    small improve on tests

--- src/DateCondition.test.ts
+++ src/DateCondition.test.ts
@@ -9,48 +9,49 @@ describe ('DateCondition', () => {
             test('empty should throw', () => {
                 expect(() => {new DateCondition({})}).toThrow()
             })
-        })
-        
-        describe('all', () => {
-            test('not array should throw', () => {
-                expect(() => {new DateCondition({ all: {} })}).toThrow()
-            })
-            test('empty array should throw', () => {
-                expect(() => {new DateCondition({ all: [] })}).toThrow()
-            })
-        })
-        describe('any', () => {
-            test('not array should throw', () => {
-                expect(() => {new DateCondition({ all: {} })}).toThrow()
-            })
-            test('empty array should throw', () => {
-                expect(() => {new DateCondition({ all: [] })}).toThrow()
-            })
-        })
-        describe('before', () => {
-            test('not string should throw', () => {
-                expect(() => {new DateCondition({ before: 0 })}).toThrow()
-            })
-            test('not string represent date should throw', () => {
-                expect(() => {new DateCondition({ before: 'abc' })}).toThrow()
-            })
-        })
-        describe('after', () => {
-            test('not string should throw', () => {
-                expect(() => {new DateCondition({ after: 0 })}).toThrow()
-            })
-            test('not string represent date should throw', () => {
-                expect(() => {new DateCondition({ after: 'abc' })}).toThrow()
-            })
-        })
-        test('has multiple things should throw', () => {
-            expect(() => {
-                new DateCondition({
-                    before: '2022-05-15T07:45:07.172Z',
-                    after: '2022-05-15T07:45:07.172Z' 
+
+            describe('all', () => {
+                test('not array should throw', () => {
+                    expect(() => {new DateCondition({ all: {} })}).toThrow()
                 })
-            }).toThrow()
+                test('empty array should throw', () => {
+                    expect(() => {new DateCondition({ all: [] })}).toThrow()
+                })
+            })
+            describe('any', () => {
+                test('not array should throw', () => {
+                    expect(() => {new DateCondition({ all: {} })}).toThrow()
+                })
+                test('empty array should throw', () => {
+                    expect(() => {new DateCondition({ all: [] })}).toThrow()
+                })
+            })
+            describe('before', () => {
+                test('not string should throw', () => {
+                    expect(() => {new DateCondition({ before: 0 })}).toThrow()
+                })
+                test('not string represent date should throw', () => {
+                    expect(() => {new DateCondition({ before: 'abc' })}).toThrow()
+                })
+            })
+            describe('after', () => {
+                test('not string should throw', () => {
+                    expect(() => {new DateCondition({ after: 0 })}).toThrow()
+                })
+                test('not string represent date should throw', () => {
+                    expect(() => {new DateCondition({ after: 'abc' })}).toThrow()
+                })
+            })
+            test('has multiple things should throw', () => {
+                expect(() => {
+                    new DateCondition({
+                        before: '2022-05-15T07:45:07.172Z',
+                        after: '2022-05-15T07:45:07.172Z' 
+                    })
+                }).toThrow()
+            })
         })
+
     })
     
     describe('check function', () => {
@@ -66,22 +67,22 @@ describe ('DateCondition', () => {
         })
         
         describe('before', () => {
-            test('before returns true', () => {
+            test('input earlier returns true', () => {
                 const cond = new DateCondition({ before: '2022-05-15T07:45:07.172Z' })
                 expect(cond.check('2022-05-14T07:45:07.172Z')).toBe(true)
             })
-            test('not before returns false', () => {
+            test('input not earlier returns false', () => {
                 const cond = new DateCondition({ before: '2022-05-15T07:45:07.172Z' })
                 expect(cond.check('2022-05-16T07:45:07.172Z')).toBe(false)
             })
         })
 
         describe('after', () => {
-            test('after returns true', () => {
+            test('input later returns true', () => {
                 const cond = new DateCondition({ after: '2022-05-15T07:45:07.172Z' })
                 expect(cond.check('2022-05-16T07:45:07.172Z')).toBe(true)
             })
-            test('not after returns false', () => {
+            test('input not later returns false', () => {
                 const cond = new DateCondition({ after: '2022-05-15T07:45:07.172Z' })
                 expect(cond.check('2022-05-15T07:45:07.172Z')).toBe(false)
             })
